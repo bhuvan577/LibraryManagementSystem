e164966d2d1b548d8bbfe1c10b1f40d0b6731e01
Bhuvaneesh <sivabhuva29gmail.com>
2025-07-30 15:44:53 +0530

    first2commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
