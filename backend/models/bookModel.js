04749476840327c41c7b0e606bd50fb0fe1a6870
Bhuvaneesh <sivabhuva29gmail.com>
2025-07-31 18:36:00 +0530

    forst2commit

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema(
   {
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,7 +3,6 @@ const { authMiddleware, isAdmin } = require("../middleware/auth");
 const Book = require("../models/Book");
 const User = require("../models/User");
 const Borrow = require("../models/borrowModel");
-const Book = require("../models/bookModel");
 
 const router = express.Router();
 
